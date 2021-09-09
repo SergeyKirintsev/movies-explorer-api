5df13ef673671a76b8766200e16c9c180a0c483b
SergeyKirintsev <kserg80@gmail.com>
2021-09-09 21:50:10 +0300

    create single route

--- app.js
+++ app.js
@@ -3,62 +3,32 @@ const helmet = require('helmet');
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const { celebrate, Joi, errors } = require('celebrate');
-
-const auth = require('./middlewares/auth');
+const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 const limiter = require('./middlewares/rate-limit');
-
 const errorsHandler = require('./middlewares/errorsHandler');
-const { NotFoundError } = require('./errors/not-found-err');
-
-const { login, createUser } = require('./controllers/users');
+const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
 
-const app = express();
-// app.set('trust proxy', 1); // Enable if you're behind a reverse proxy
-
-app.use(requestLogger);
-app.use(limiter);
-app.use(express.json());
-app.use(cookieParser());
-app.use(helmet());
-app.use(cors);
-
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-    name: Joi.string().required(),
-  }),
-}), createUser);
-
-app.use(auth);
-
-app.use('*', () => {
-  throw new NotFoundError('Страница не найдена');
-});
-
+const app = express();
+// app.set('trust proxy', 1); // Enable if you're behind a reverse proxy
+app.use(requestLogger);
+app.use(limiter);
+app.use(express.json());
+app.use(cookieParser());
+app.use(helmet());
+app.use(cors);
+app.use(router);
 app.use(errorLogger);
-
 app.use(errors()); // обработчик ошибок celebrate
-
 app.use(errorsHandler);
-
 app.listen(PORT);
---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const auth = require('../middlewares/auth');
+const { login, createUser } = require('../controllers/users');
+const { NotFoundError } = require('../errors/not-found-err');
+
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().required(),
+  }),
+}), createUser);
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+router.use(auth);
+
+router.use('*', () => {
+  throw new NotFoundError('Страница не найдена');
+});
+
+module.exports = router;
